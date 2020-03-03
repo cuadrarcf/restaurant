e0d1a2a6e51cb8baead58b675411bcbe7966ff20
Roberley Cuadra <roberleyc91@gmail.com>
2020-03-03 18:03:12 -0500

    loading data

--- src/modules/ingredients/index.tsx
+++ src/modules/ingredients/index.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { AppContent } from '../../app/view/appContent';
 import { Button, Col, Row } from 'antd';
