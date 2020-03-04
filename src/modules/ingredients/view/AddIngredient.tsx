c906b64a4ca2ba0f4f8de1d74450178515cffd24
Roberley Cuadra <roberleyc91@gmail.com>
2020-03-04 18:22:32 -0500

    add multiple selection
    Signed-off-by: Roberley Cuadra <roberleyc91@gmail.com>

--- src/modules/ingredients/view/AddIngredient.tsx
+++ src/modules/ingredients/view/AddIngredient.tsx
@@ -1,57 +1,58 @@
 import React from 'react';
-import { Modal, Form, Input, Select } from 'antd';
+import { Modal, Form, Input, Select, InputNumber, Tag } from 'antd';
 
-const { Option } = Select;
 export interface IAddProps {
   isVisible: boolean;
 }
 
-export class AddIngredient extends React.Component<IAddProps, {}> {
+const layout = {
+  labelCol: { span: 8 }
+};
+
+export interface IIngredientModel {
+  name: string;
+  qty: number;
+  color: string[];
+}
+
+export class AddIngredient extends React.Component<IAddProps, IIngredientModel> {
+
   formRef: any = React.createRef();
 
   onFinish = (values: any) => {
+
     console.log('Success:', values);
   };
 
   render() {
-
     const { isVisible } = this.props;
 
     return (
-      <Modal
-        title="Add Item"
-        visible={isVisible}
-        onOk={()=>this.formRef.current.submit()}
-      >
-        <Form ref={this.formRef} onFinish={this.onFinish}>
+      <Modal title="Add Ingredient" visible={isVisible} onOk={() => this.formRef.current.submit()}>
+        <Form {...layout} ref={this.formRef} name="basic" initialValues={{ remember: true }} onFinish={this.onFinish}>
 
-          <Form.Item
-            name="note"
-            label="Note"
-            rules={[
-              {
-                required: true
-              }
-            ]}
-          >
+          <Form.Item label="Name" name="name" rules={[{ required: true, message: 'Please input the name!'}]}>
             <Input />
           </Form.Item>
 
-          <Form.Item
-            name="gender"
-            label="Gender"
-            rules={[
-              {
-                required: true
-              }
-            ]}
-          >
-            <Select placeholder="Select a option and change input text above"allowClear>
-              <Option value="male">male</Option>
-              <Option value="female">female</Option>
-              <Option value="other">other</Option>
+          <Form.Item label="Quantity" name="qty" rules={[{ required: true, message: 'Please input the Quantity!'}]}>
+            <InputNumber />
+          </Form.Item>
+
+          <Form.Item label="Color" name="color" rules={[{ required: true, message: 'Please input the Color!'}]}>
+            <Select style={{ width: 171 }} mode="multiple">
+              <Select.Option value="blue">
+                <Tag color={'blue'}>Blue</Tag>
+              </Select.Option>
+              <Select.Option value="red">
+                <Tag color={'red'}>Red</Tag>
+              </Select.Option>
+              <Select.Option value="green">
+                <Tag color={'green'}>Green</Tag>
+              </Select.Option>
             </Select>
           </Form.Item>
+
         </Form>
       </Modal>
     );
